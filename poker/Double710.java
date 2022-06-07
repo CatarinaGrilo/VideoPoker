fbb205627637042240d47f7937a50cc7e202ca3e
CatarinaGrilo <anacatarinagrilo2012@gmail.com>
2022-06-07 14:52:19 +0100

    testing push

---
+++ poker/Double710.java
@@ -0,0 +1,25 @@
+package VideoPoker.poker;
+
+
+
+/**
+ * Class that implements the Double Bonus 7/10 variant with the
+ * use of interface GameType.
+ * @author Ana Catarina Grilo, Maragrida Fernandes, Mónica Gomez
+ *
+ */
+public class Double710 implements GameType{
+
+
+    public String evaluation(Card[] cards){
+
+    }
+    public int valueOfHand(String whichhand, int bet){
+
+    }
+    public int[] advice(Card[] cards){
+        
+    }
+
+    
+}
