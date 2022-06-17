9b5bc79d18238dce80d0d661e36cbe52d2e84b40
CatarinaGrilo <anacatarinagrilo2012@gmail.com>
2022-06-17 13:25:53 +0100

    missing errors dections

--- poker/Main.java
+++ poker/Main.java
@@ -4,13 +4,8 @@ public class Main {
 
         public static void main(String[] args) {
 
-                // char mode = 's'; // Stores wich mode it is to run
-                // int money = 10; // Stores the inicial money of the player
-
                 if (args[0].equals("-d")) {
 
-                        // String cmds = "VideoPoker/Input/test1_cmd.txt";
-                        // String deckInput = "VideoPoker/Input/test1_deck.txt";
                         Debug game = new Debug(Integer.parseInt(args[1]), args[2], args[3]);
 
                         // Process of accessing the commands in mode Debug
@@ -52,8 +47,7 @@ public class Main {
                                         while (j != i) {
                                                 aux = game.commands.get(0);
                                                 game.commands.remove(0);
-                                                aux3[j] = Integer.parseInt(aux) - 1; // -1 to be position of array from
-                                                                                     // 0 to 4
+                                                aux3[j] = Integer.parseInt(aux) - 1; // -1 to be index
                                                 j++;
                                                 str += aux + " ";
                                         }
@@ -74,14 +68,12 @@ public class Main {
                         }
                 } else if (args[0].equals("-s")) {
 
-                        // int playerBet = 3;
-                        int nbdeals = 10;
                         int i = 0;
                         boolean flag = true;
                         Simulation game = new Simulation(Integer.parseInt(args[1]), Integer.parseInt(args[2]),
-                                        nbdeals = Integer.parseInt(args[3]));
+                                        Integer.parseInt(args[3]));
 
-                        while (i < nbdeals && flag == true) {
+                        while (i < game.nbdeals && flag == true) {
                                 System.out.print("nbdeal:" + i + "\n");
                                 flag = game.bet();
                                 if (flag == false)
